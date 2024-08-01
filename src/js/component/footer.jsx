ef76392140b38471a7c67aa5bf80e3aa8cba8ee4
Gabrielviscio10 <gonzalo.viscio@icloud.com>
2024-08-01 18:29:09 +0000

    a la mitad

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,1 +1,9 @@
 import React from "react";
+const Footer = () => {
+    return (
+        <div className="bg-dark text-white text-center p-3 mt-4">
+            <p>© 2024 Your Company. All Rights Reserved.</p>
+        </div>
+    );
+};
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,22 +4,26 @@ import React from "react";
 import NavBar from "./nav.jsx";
 import Jumbotron from "./Jumbotron.jsx"
 import Card from "./card.jsx";
+import Footer from "./footer.jsx";
 
 
 //create your first component
 const Home = () => {
-	return (
-		<div>
-			<NavBar/>
-			<div className="container">
-				<Jumbotron/>
-				<div className="card-container row">
-					<Card/><Card/><Card/><Card/>
-				</div>
-			</div>
-		</div>
-		
-	);
+    return (
+        <div> <div className="text-end">
+                <NavBar/>
+                </div>
+            <div className="container">
+                <div className="bg-light pb-5"> {/* Cambié bg-secondary por bg-light */}
+                    <Jumbotron/>
+                </div>
+                <div className="container-fluid row justify-content-between">
+                    <Card/><Card/><Card/><Card/>
+                </div>
+            </div>
+            <Footer/>
+        </div>
+    );
 };
 
 export default Home;
